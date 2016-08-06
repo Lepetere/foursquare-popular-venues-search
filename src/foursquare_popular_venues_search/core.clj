b5719038bd1a71e1d5c1a9b7eeb7128117cf5c50
Peter Fessel <peter.fessel@live.de>
2016-08-06 11:47:59 +0100

    send basic explore request to foursquare and output result

--- src/foursquare_popular_venues_search/core.clj
+++ src/foursquare_popular_venues_search/core.clj
@@ -1,7 +1,19 @@
 (ns foursquare-popular-venues-search.core
+  (:require [clj-http.client :as client])
+  (:use [cheshire.core :only [parse-string]])
+  (:use clojure.pprint)
   (:gen-class))
 
+(def client-id "0PQJ1WYAFXCPN2IPBLN4ABB2X2DIQVDLK2XCKCAS4555LMVK")
+(def client-secret "3XN0Z24WDBDI0AMZ2MUQIXMPGJNH11JC4LSQRMTOMDV3CTIR")
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [response (client/get "https://api.foursquare.com/v2/venues/explore"
+          {:query-params {
+            "client_id" client-id,
+            "client_secret" client-secret,
+            "v" "20130815",
+            "near" "Chicago, IL"
+            }})]
+      (pprint (parse-string (get response :body)))))
