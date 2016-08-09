0f9a5d4c21e817d332505d753941d3274427d36b
Peter Fessel <peter.fessel@live.de>
2016-08-09 10:01:31 +0100

    receive location as url parameter and return foursquare recommended places as json

--- src/foursquare_popular_venues_search/core.clj
+++ src/foursquare_popular_venues_search/core.clj
@@ -1,8 +1,8 @@
 (ns foursquare-popular-venues-search.core
   (:require [clj-http.client :as client])
   (:use [ring.middleware.json :only [wrap-json-response]])
+  (:use [ring.middleware.params :only [wrap-params]])
   (:use [ring.util.response :only [response]])
-  (:use clojure.pprint)
   (:use [cheshire.core :only [parse-string]])
   (:use ring.adapter.jetty)
   (:gen-class))
@@ -10,20 +10,26 @@
 (def client-id "0PQJ1WYAFXCPN2IPBLN4ABB2X2DIQVDLK2XCKCAS4555LMVK")
 (def client-secret "3XN0Z24WDBDI0AMZ2MUQIXMPGJNH11JC4LSQRMTOMDV3CTIR")
 
-(defn handler [request]
-  (response {:foo "bar"}))
-
-(def app
-  (wrap-json-response handler))
-
-(defn -main
-  [& args]
-  (let [response (client/get "https://api.foursquare.com/v2/venues/explore"
+(defn perform-foursquare-explore-request
+  "Performs a request to the explore endpoint of the foursquare api.
+  Returns recommended and popular venues near a given (geocodable) location."
+  [location]
+  (parse-string (:body (client/get "https://api.foursquare.com/v2/venues/explore"
           {:query-params {
             "client_id" client-id,
             "client_secret" client-secret,
             "v" "20130815",
-            "near" "Chicago, IL"
-            }})]
-      (pprint (parse-string (get response :body))))
+            "near" location
+            }}))))
+
+(defn handler [request]
+  (let [location (get (:query-params request) "location")]
+    (response (perform-foursquare-explore-request location))))
+
+(def app
+  ;; apply middleware to correctly handle json and url params
+  (wrap-params (wrap-json-response handler)))
+
+(defn -main
+  [& args]
   (run-jetty app {:port 8080}))
